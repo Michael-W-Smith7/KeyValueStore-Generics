4e08f46ba638a8cc084d42fe63a1da5be9d15fa8
Michael <Michael.W.Smith77@outlook.com>
2019-09-19 08:54:36 -0400

    Added all files

---
+++ KeyValue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_Value_Store
+{
+    public struct KeyValue<t>
+    {
+        public string Key;
+        public t Value;
+
+        
+        public KeyValue(string _key, t _val)
+        {
+            Key = _key;
+            Value = _val;
+        }
+    }
+}
---
+++ MyDictionary.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_Value_Store
+{
+    public class MyDictionary
+    {
+        
+        public KeyValue<object>[] DictionArray = new KeyValue<object>[]
+        {
+            new KeyValue<object>("NotDog",  1),
+            new KeyValue<object>("Cats", 2),
+            new KeyValue<object>("Penguin", 4)
+        };
+
+         public object this [string _Key]
+        {
+            get
+            {
+                
+                foreach (var Z in DictionArray)
+                {
+                    if (Z.Key.Equals(_Key))
+                    {
+                        return Z.Value;
+                    }
+                }
+                throw new KeyNotFoundException ($"Key not found with the value {_Key}");
+            }
+            set
+            {
+                bool exists = false;
+                int i = 0, temp = 0;
+                foreach (var Z in DictionArray)
+                {
+                    if (Z.Key.Equals(_Key))
+                    {
+                        exists = true;
+                        temp = i;
+                    }
+                    i++;
+                }
+                if (exists == true)
+                {
+                    DictionArray[temp] = new KeyValue<object>(_Key, value);
+                }
+                else
+                {
+                    Array.Resize(ref DictionArray, (DictionArray.Length + 1));
+                    DictionArray[DictionArray.Length-1] = new KeyValue<object>(_Key, value);
+                }
+            }
+        }
+
+    }
+}
